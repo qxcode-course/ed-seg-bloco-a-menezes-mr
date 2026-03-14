3f7e503b66313619c1837e9d1e0fb538e95458b2
Menezes <menzs.marcelo@gmail.com>
2026-03-14 17:41:41 -0300

    finalizando - mumia

--- ed/mumia/go/draft.go
+++ ed/mumia/go/draft.go
@@ -1,5 +1,22 @@
 package main
 import "fmt"
 func main() {
-    fmt.Println("qxcode")
+
+    var nome string
+    var id int
+
+    fmt.Scanf("%s %d", nome, id)
+
+    if id < 12{
+        fmt.Printf("%s eh crianca\n", nome)
+    }else if id < 18{
+        fmt.Printf("%s eh jovem\n", nome)
+    }else if id < 65{
+        fmt.Printf("%s eh adulto\n", nome)
+    }else if id < 1000{
+        fmt.Printf("%s eh idoso\n", nome)
+    }else {
+        fmt.Printf("%s eh mumia\n", nome)
+    }
+    
 }
