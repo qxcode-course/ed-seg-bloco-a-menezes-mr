fe420f146cad0b1a33b82699af9c6b3173ac9b94
Menezes <menzs.marcelo@gmail.com>
2026-03-14 19:24:23 -0300

    finalizando - pedra

--- ed/pedra/go/draft.go
+++ ed/pedra/go/draft.go
@@ -1,5 +1,40 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+	"math"
+)
 func main() {
-    fmt.Println("qxcode")
+    var n, a, b int
+    fmt.Scan(&n)
+    jogo := make([]int, n)
+
+    for i := 0; i < n; i++ {
+
+        fmt.Scan(&a, &b)
+        if a < 10 || b < 10 {
+            jogo[i] = -1
+        } else  {
+            jogo[i] = int(math.Abs(float64(a - b)))
+        }
+    }
+
+    auxId := -1
+    auxDif := 99999
+    
+    for id, competidor := range jogo{
+
+        if competidor != -1 && competidor < auxDif {
+            auxId = id
+            auxDif = competidor
+        }
+
+    }
+
+    if auxId == -1 {
+        fmt.Println("sem ganhador")
+    } else {
+        fmt.Println(auxId)
+    }
+    
 }
