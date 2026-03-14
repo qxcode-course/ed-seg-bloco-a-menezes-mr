6f7b20e6f1d436121101a7cb3489a4f97a944a01
Menezes <menzs.marcelo@gmail.com>
2026-03-14 19:52:03 -0300

    finlizando - casal

--- ed/casais/go/draft.go
+++ ed/casais/go/draft.go
@@ -1,5 +1,32 @@
 package main
+
 import "fmt"
+
 func main() {
-    fmt.Println("qxcode")
+
+    var n, t int
+    fmt.Scan(&n)
+    casais := make([]int, n)
+
+    for i := 0; i < n; i++ {
+        var a int
+        fmt.Scan(&a)
+        casais[i] = a
+    }
+
+    for i := 0; i < n; i++ {
+        if casais[i] == 0 {
+            continue
+        }
+        for j := i+1; j < n; j++ {
+            if casais[i] == -casais[j]{
+                t++
+                casais[j] = 0
+                casais[j] = 0
+                break
+            }
+        }
+    }
+
+    fmt.Println(t)
 }
