871cbe0342ac136891dd26bceb59fd4d66eef068
Menezes <menzs.marcelo@gmail.com>
2026-03-14 18:32:35 -0300

    finalizando - fuga

--- ed/fuga/go/draft.go
+++ ed/fuga/go/draft.go
@@ -1,5 +1,22 @@
 package main
+
 import "fmt"
+
 func main() {
-    fmt.Println("qxcode")
+    var h, p, f, d int
+    fmt.Scan(&h, &p, &f, &d)
+    
+    for {
+
+        if f == h {
+            fmt.Println("S")
+            break
+        }
+        if f == h {
+            fmt.Println("N")
+            break
+        }
+
+        f = ((f+d)%16 + 16) % 16
+    }
 }
