e6a26b30607484cd6ef1a770e9f22f02d2aba55a
Menezes <menzs.marcelo@gmail.com>
2026-03-15 09:50:59 -0300

    finalizando - baruel

--- ed/baruel/go/draft.go
+++ ed/baruel/go/draft.go
@@ -1,5 +1,48 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+	"strconv"
+)
+
 func main() {
-    fmt.Println("qxcode")
+
+	var n, x, y int
+	var rep string = ""
+	var fal string = ""
+
+	fmt.Scan(&n, &x)
+	album := make([]int, n)
+
+	for i := 0; i < x; i++ {
+		fmt.Scan(&y)
+		album[y-1]++
+	}
+
+	for i := 0; i < n; i++ {
+		if album[i] != 0 {
+
+			for j := 0; j < album[i]-1; j++ {
+				if rep != "" {
+					rep += " "
+				}
+				rep += strconv.Itoa(i+1)
+			}
+		} else {
+			if fal != "" {
+				fal += " "
+			}
+			fal += strconv.Itoa(i+1)
+		}
+	}
+
+	if rep == "" {
+		rep = "N"
+	}
+	if fal == "" {
+		fal = "N"
+	}
+
+	fmt.Println(rep)
+	fmt.Println(fal)
 }
