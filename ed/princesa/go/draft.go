0ef9e21ced0e73858e662d9d92608c14981589c5
Menezes <menzs.marcelo@gmail.com>
2026-03-23 18:44:40 -0300

    logica do jogo

--- ed/princesa/go/draft.go
+++ ed/princesa/go/draft.go
@@ -1,5 +1,31 @@
 package main
 import "fmt"
+
 func main() {
-    fmt.Println("qxcode")
+
+    var n, e int
+    fmt.Scan(&n, &e)
+    combate := make([]int, n)
+    aux01 := e - 1
+    aux02 := e % n
+    size := n
+
+
+    for size > 1 {
+        if combate[aux01] == 1 {
+            aux01 = (aux01 + 1) % n
+            continue
+        }
+        if combate[aux02] == 1 || aux02 == aux01 {
+            aux02 = (aux02 + 1) % n
+            continue
+        }
+
+        combate[aux02] = 1
+        size--;
+
+        aux01 = (aux02 + 1) % n
+        aux02 = (aux01 + 1) % n
+
+    }
 }
