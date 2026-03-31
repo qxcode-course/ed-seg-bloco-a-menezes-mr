ff84015a5f310d0fb18269768b34d57748881b32
Menezes <menzs.marcelo@gmail.com>
2026-03-31 17:02:10 -0300

    construindo - apagando

--- ed/apagando/go/draft.go
+++ ed/apagando/go/draft.go
@@ -1,4 +1,25 @@
 package main
 import "fmt"
 func main() {
+	var n, qtd, ps int
+	fmt.Scan(&n)
+	fila := make([]int, n)
+
+	for _, pessoa := range fila{
+		fmt.Scan(&pessoa)
+	}
+
+	fmt.Scan(&qtd)
+	saiu := make(map[int]bool)
+
+	for range qtd {
+		fmt.Scan(&ps)
+		saiu[ps] = true
+	}
+
+	filaNova := make([]int, n - qtd)
+	for i := 0; i < n - qtd; i++ {
+
+	}
+
 }
