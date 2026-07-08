e047136587d999fc4059dbbc6865c8f2f4ca4fdc
Marcelo Menezes <menzs.marcelo@gmail.com>
2026-07-07 21:35:27 -0300

    cercado

--- ed/cercado/src/go/main.go
+++ ed/cercado/src/go/main.go
@@ -6,9 +6,54 @@ import (
 	"os"
 )
 
+func marcar(grid [][]byte, l, c int) {
+	if l < 0 || l >= len(grid) || c < 0 || c >= len(grid[0]) || grid[l][c] != 'O' {
+		return
+	}
+
+	grid[l][c] = '#'
+
+	marcar(grid, l - 1, c)
+	marcar(grid, l + 1, c)
+	marcar(grid, l, c - 1)
+	marcar(grid, l, c + 1)
+
+}
 // NÃO ALTERE A ASSINATURA DA FUNÇÃO solve
 func solve(board [][]byte) {
-	_ = board
+	nl := len(board)
+	nc := len(board[0])
+
+	for i := range nl {
+		if board[i][0] == 'O' {
+			marcar(board, i, 0)
+		}
+
+		if board[i][nc - 1] == 'O' {
+			marcar(board, i, nc - 1)
+		}
+	}
+
+	for i := range nc {
+		if board[0][i] == 'O' {
+			marcar(board, 0, i)
+		}
+
+		if board[nl - 1][i] == 'O' {
+			marcar(board, nl - 1, i)
+		}
+	}
+
+	for i := range nl {
+		for j := range nc {
+			if board[i][j] == 'O' {
+				board[i][j] = 'X'
+			}else if board[i][j] == '#' {
+				board[i][j] = 'O'
+			}
+		}
+	}
+
 }
 
 // NÃO ALTERE A MAIN
